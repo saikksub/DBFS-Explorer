c391821df54b677c16cbdab7e11783010a986e06
Sai K K <saikksub@gmail.com>
2019-11-07 09:07:55 +0530

    cleanup dependencies

--- src/main/index.js
+++ src/main/index.js
@@ -3,10 +3,8 @@
 import {
   app,
   BrowserWindow,
-  ipcMain,
   Menu
 } from 'electron'
-import Sqlite3 from './database'
 import { autoUpdater } from 'electron-updater'
 
 // Import application menu configuration
@@ -14,7 +12,6 @@ import appMenu from './menu/menu.js'
 import macMenu from './menu/macMenu.js'
 
 const os = require('os')
-const path = require('path')
 
 // Set about panel for macOS. This will be ignored for windows
 if (os.platform() === 'darwin') {
@@ -52,18 +49,6 @@ if (process.platform === 'darwin') {
 // Build electron Menu object from menu template
 const menu = Menu.buildFromTemplate(appMenu)
 
-// Get SQL database Instance
-const db = new Sqlite3({
-  path: path.join(
-    os.homedir(),
-    'DBFS-Explorer'
-  ),
-  name: 'app'
-})
-
-// Initialize SQL database
-db.init()
-
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
@@ -84,6 +69,7 @@ const winURL = process.env.NODE_ENV === 'development'
  * Creates default application window
  */
 function createWindow () {
+  console.log('createWindow')
   /**
    * Initial window options
    */
@@ -130,77 +116,6 @@ app.on('activate', () => {
     createWindow()
   }
 })
-
-/**
- * Listen to the channel "sql", when a new message arrives listener would be called
- * with event, sql CRUD configuration
- */
-
-ipcMain.on('sql', function (event, config) {
-  // Validate SQL CRUD configuration
-  if (config && config.constructor === {}.constructor &&
-    'name' in config && 'data' in config &&
-    'commit' in config) {
-    // CRUD operation type
-    switch (config.name) {
-      /**
-       * Read entire rows of specified table
-       */
-      case 'readFullTable':
-        db.readFullTable(config.data, function (error, data) {
-          replaySender(event.sender, config.commit, error, data)
-        })
-        break
-
-      /**
-       * Read table with entry/row id
-       */
-      case 'readTableEntryID':
-        db.readTableEntryID(config.data, function (error, data) {
-          replaySender(event.sender, config.commit, error, data)
-        })
-        break
-
-      /**
-       * Write a new entry/row of specified table
-       */
-      case 'writeTable':
-        db.writeTable(config.data, function () {})
-        break
-
-      /**
-       * Update table row of specified id
-       */
-      case 'UpdateTableByID':
-        db.updateTableByID(config.data, function (error, data) {
-          replaySender(event.sender, config.commit, error, data)
-        })
-        break
-
-      /**
-       * Delete table row of specified id
-       */
-      case 'DeleteTableById':
-        db.deleteTableById(config.data, function (error, data) {
-          replaySender(event.sender, config.commit, error, data)
-        })
-        break
-      default: break
-    }
-  }
-})
-
-/**
- * Replay sender with SQL resultant data, error (if any)
- * and commit string for vuex store
- */
-function replaySender (sender, commit, error, data) {
-  sender.send('sql_ready', {
-    commit: commit, // Vuex commit message
-    error: error, // Error object
-    data: data // Resultant data
-  })
-}
 
 /**
  * Add shortcut callback to the application menu
